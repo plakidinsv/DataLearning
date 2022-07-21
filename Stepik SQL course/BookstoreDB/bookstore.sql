128440b39f320db955e5f9d7e487299db1f1f43c
plakidinsv <90641193+plakidinsv@users.noreply.github.com>
2022-07-21 14:06:10 +0300

    q

--- Stepik SQL course/BookstoreDB/bookstore.sql
+++ Stepik SQL course/BookstoreDB/bookstore.sql
@@ -224,3 +224,28 @@ HAVING SUM(buy_book.amount) =
                 JOIN buy_book USING(book_id)
              group by genre_id) as tmp)
     
+
+
+DROP TABLE IF EXISTS buy_archive;
+CREATE TABLE buy_archive
+(
+    buy_archive_id INT PRIMARY KEY AUTO_INCREMENT,
+    buy_id         INT,
+    client_id      INT,
+    book_id        INT,
+    date_payment   DATE,
+    price          DECIMAL(8, 2),
+    amount         INT
+);
+
+INSERT INTO buy_archive (buy_id, client_id, book_id, date_payment, amount, price)
+VALUES (2, 1, 1, '2019-02-21', 2, 670.60),
+       (2, 1, 3, '2019-02-21', 1, 450.90),
+       (1, 2, 2, '2019-02-10', 2, 520.30),
+       (1, 2, 4, '2019-02-10', 3, 780.90),
+       (1, 2, 3, '2019-02-10', 1, 450.90),
+       (3, 4, 4, '2019-03-05', 4, 780.90),
+       (3, 4, 5, '2019-03-05', 2, 480.90),
+       (4, 1, 6, '2019-03-12', 1, 650.00),
+       (5, 2, 1, '2019-03-18', 2, 670.60),
+       (5, 2, 4, '2019-03-18', 1, 780.90);
