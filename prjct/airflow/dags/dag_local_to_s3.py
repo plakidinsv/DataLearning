c290b7f0d33308e9de0b3a4c6c5aec32b4adabec
plakidinsv <90641193+plakidinsv@users.noreply.github.com>
2022-09-19 11:55:27 +0300

    up

--- prjct/airflow/dags/dag_local_to_s3.py
+++ prjct/airflow/dags/dag_local_to_s3.py
@@ -1,4 +1,5 @@
 import os
+from cloudpathlib import CloudPath
 from datetime import datetime
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
@@ -21,13 +22,24 @@ add tasks:
 '''
 
 def upload_file(ds, **kwargs):
-    # Upload files from local to Minio
+    # Upload files from local to MinIO
     source = os.listdir('./Source') # вынести в переменную
     for file in source:
         s3 = S3Hook('minio_conn')
         s3.load_file(f'./Source/{file}',
                      key=file,
                      bucket_name='prjct.raw.data')
+
+
+def copy_file(ds, **kwarfs):
+    # Copy files from raw bucket MinIO to transformation bucket MinIO
+    source_bucket = CloudPath('s3://prjct.raw.data/')
+    for file in source_bucket:                         # ERROR - Failed to execute job 37 for task copy_file_to_transformation_bucket ('S3Path' object is not iterable; 1062)
+        s3 = S3Hook('minio_conn')
+        s3.copy_object(source_bucket_key=file,
+                        dest_bucket_key=file,
+                        source_bucket_name='prjct.raw.data',
+                        dest_bucket_name='prjct.transfom.bucket')
 
 with DAG (dag_id='load_local_to_minio',
         start_date=datetime(2022, 8, 25),  
@@ -44,3 +56,10 @@ with DAG (dag_id='load_local_to_minio',
         python_callable=upload_file
     )
 
+    t2 =PythonOperator(
+        task_id='copy_file_to_transformation_bucket',
+        provide_context=True,
+        python_callable=copy_file
+    )
+
+t1 >> t2
